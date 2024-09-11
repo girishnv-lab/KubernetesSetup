332d50a8f3057b63755c9dbe3405c0d9c603923d
girishnv-lab <135215839+girishnv-lab@users.noreply.github.com>
2024-09-11 17:01:17 +0530

    Update k8onall.sh

--- k8onall.sh
+++ k8onall.sh
@@ -36,7 +36,7 @@ elif [ -f /etc/amazon-linux-release ]; then
     # For Amazon Linux systems (2023 and 2)
     echo "Detected Amazon Linux system"
     sudo yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
-    sudo amazon-linux-extras install -y docker
+    sudo yum install -y docker
     sudo systemctl start docker
     sudo systemctl enable docker
 
