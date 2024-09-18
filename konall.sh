505e259422e1423f9fb980996e410f1776a90331
girishnv-lab <135215839+girishnv-lab@users.noreply.github.com>
2024-09-18 13:27:21 +0530

    Update konall.sh

--- konall.sh
+++ konall.sh
@@ -64,9 +64,9 @@ sudo install -o root -g root -m 0755 kubectl /usr/bin/kubectl
 
 # Installing Kind cluster
 echo "Installing Kind cluster..."
-curl -Lo kind-config.yaml https://github.com/girishnv-lab/KubernetesSetup/blob/main/kind-config.yaml
+curl -Lo kind-new.yaml https://github.com/girishnv-lab/KubernetesSetup/blob/main/kind-config.yaml
 echo "Creating Kind cluster with custom configuration..."
-kind create cluster --config kind-config.yaml --retain --image "kindest/node:v1.21.14"
+kind create cluster --config kind-new.yaml --retain --image "kindest/node:v1.21.14"
 
 kubectl cluster-info --context kind-kind
 
